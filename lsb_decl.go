bece188d46c5487f412a6dfad9cb51e3f7dfe505
esp <esp@liferoot.ru>
2016-08-31 01:12:00 +0300

    fix comments for the LSB* decl.

--- lsb_decl.go
+++ lsb_decl.go
@@ -4,15 +4,15 @@ package bits
 
 //go:noescape
 
-// LSB64 returns the index of the Most Significant Bit of an unsigned 64-bit integer.
+// LSB64 returns the index of the Least Significant Bit of an unsigned 64-bit integer.
 func LSB64(x uint64) uint64
 
 //go:noescape
 
-// LSB32 returns the index of the Most Significant Bit of an unsigned 32-bit integer.
+// LSB32 returns the index of the Least Significant Bit of an unsigned 32-bit integer.
 func LSB32(x uint32) uint32
 
 //go:noescape
 
-// LSB16 returns the index of the Most Significant Bit of an unsigned 16-bit integer.
+// LSB16 returns the index of the Least Significant Bit of an unsigned 16-bit integer.
 func LSB16(x uint16) uint16
